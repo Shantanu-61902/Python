fbb7348400d85c2db45f4b3d95b4387eb9f3b96e
Shantanu Mishra <119602419+Shantanu-61902@users.noreply.github.com>
2024-03-22 21:25:28 +0530

    Add files via upload
    Python Basics

---
+++ index.py
@@ -0,0 +1,60 @@
+print('Hello World')
+# this is comment
+
+x = 5;
+X = 633;
+print(x)
+print(X)
+print(type(x))
+y = "John Cena";
+print(type(y))
+
+a,b,c = 34 , "Roman" , 78
+print(a)
+
+print(b)
+
+print(c)
+
+m = n = o = "Virat Kohli"
+print(m)
+print(n)
+print(o)
+
+a = "Welcome to"
+b = "30 Days of coding"
+print(a+" "+b)
+
+# arithmetic exp
+#x = 10
+#y = 5
+#print(x + y)
+#print(x - y)
+#print(x * y)
+#print(x / y)
+#print(x % y)
+#print(x ** y)
+#print(x // y)
+
+# Assignment Operator------
+#x = 1;
+#x += 1;
+#print(x)
+
+#Comparison Operator
+#x = 5;
+#y = 7;
+#print(x == y);
+#print(x != y);
+#print(x <= y);
+#print(x >= y);
+#print(x > y);
+#print(x < y);
+
+#Logical Operator
+#print(5>2 and 2>5)
+#print(5>2 or 5<2)
+
+#print(not(True))
+#print(not(False))
+
